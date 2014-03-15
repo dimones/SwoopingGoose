96166c259d1a1e545945f3ab5aec169eac0d35f3
Dimones <Dimones951@yandex.ru>
2014-03-16 00:08:08 +0700

    Удалена физика плеера
    Полностью удалена физика,старые скрипты. Решена проблема с Collider2D,
    заменено на Collision2D

--- assets/Scripts/SGGooseController.cs
+++ assets/Scripts/SGGooseController.cs
@@ -7,18 +7,14 @@ public class SGGooseController : MonoBehaviour {
     public int _curScore = 0;//текущий счет
     public int _maxScore = 0;//максимальный,служит для хранения
     public int _maxReplies = 0;//На будущее для статистики кто сколько попыток совершал.
-    public float _curfirst = 1800f;//сила первого раза
-    public float _curSecond = 3500f;//сила второго нажатия
-    public float _gooseSpeed = 3.5f;//скорость гуся
     #endregion
-    private Rigidbody2D _body;//тело
     private Camera _camera;
     public bool _firstTouch = false; //для изначального несильного толка без компенсации
     public State state = State.Prepare;//Типа стадии игры
     public Direction _dir = Direction.Right;
     public float _PosX; //Позиция с течением времени
     //public System.Random _rand = new System.Random();  Рандом для генерации
-    public KeyCode _code;//Изменение кнопки полета для удобства на пк
+    public static KeyCode _code;//Изменение кнопки полета для удобства на пк
     public int _CurStage = 0;
 
     public enum Direction
@@ -34,52 +30,15 @@ public class SGGooseController : MonoBehaviour {
         Lose
     }
 	void Start(){
-        _body = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        _body.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update()
-    {        
-        #region Контроль движения
-        if (_firstTouch == true)
-            state = State.Game;
-        #region TouchEvents
-        if (Input.touchCount > 0)
-        {
-            if ((Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetKeyDown(KeyCode.Space)))
-            {
-                _body.isKinematic = false;
-                if (_firstTouch == false)
-                {
-                    _body.AddForce(Vector2.up * _curSecond);
-                }
-                else _body.AddForce(Vector2.up * (_curSecond + _curfirst * 2));
-                _firstTouch = true;
-            }
-        }
-        #endregion
-        #endregion
+    {    
     }
-    void FixedUpdate()
-    {
-        #region Аналогично из функции Update только для SPACE
-        if (_firstTouch == true)
-            state = State.Game;
-        if ((Input.GetKeyDown(KeyCode.Space)))
-        {
-            _body.isKinematic = false;
-            if (_firstTouch == false)
-            {
-                _body.AddForce(Vector2.up * _curSecond);
-            }
-            else _body.AddForce(Vector2.up * (_curSecond + _curfirst * 2));
-            _firstTouch = true;
-        }
-        #endregion
-    }
-    void OnColliderEnter2D(Collider other){
+
+	void OnCollisionEnter2D(Collision2D coll) {
         state = State.Lose;
     }
     void OnGUI()
@@ -87,6 +46,7 @@ public class SGGooseController : MonoBehaviour {
         //специально сделал для адекватного вида всего этого
         if (state == State.Prepare)
         {
+			_firstTouch = false;
             Prepare();
         }
         else if (state == State.Generate)
@@ -100,28 +60,43 @@ public class SGGooseController : MonoBehaviour {
         else if (state == State.Lose)
         {
             Lose();
+			this.transform.position = new Vector2(this.transform.position.x-0.2f,this.transform.position.y);
+			this.GetComponent<Displacement>().enabled = false;
+			this.GetComponent<TestSGController>().enabled = false;
+			//Destroy(this.GetComponent<Displacement>());
+
         }
     }
+	void FixedUpdate()
+	{
+		#region Аналогично из функции Update только для SPACE
+		if (_firstTouch == true)
+		{
+			state = State.Game;
+			this.GetComponent<Displacement>().enabled = true;
+			this.GetComponent<TestSGController>().enabled = true;
+		}
+		if ((Input.GetKeyDown(KeyCode.Space)))
+		{
+			_firstTouch = true;
+		}
+		#endregion
+		}
 
     private void Game()
     {
         if (_dir == Direction.Right)
         {
-            _camera.transform.position = new Vector3(_body.transform.position.x + 6.5f, _camera.transform.position.y, -10.0f);
-            _PosX += (float)Time.deltaTime * _gooseSpeed;
-            _body.transform.localPosition = new Vector2(_PosX, _body.transform.position.y);
+            _camera.transform.position = new Vector3(this.transform.position.x +6f, _camera.transform.position.y, -10.0f);
         }
         if (_dir == Direction.Left)
         {
-            _camera.transform.position = new Vector3(_body.transform.position.x-6.5f, _camera.transform.position.y,-10.0f);
-            _PosX -= (float)Time.deltaTime * _gooseSpeed;
-            _body.transform.localPosition = new Vector2(_PosX, _body.transform.position.y);
+            _camera.transform.position = new Vector3(this.transform.position.x-6f, _camera.transform.position.y,-10.0f);
         }
     }
 
     private void Lose()
     {
-        _body.isKinematic = true;
         if (GUI.Button(new Rect(Screen.width / 2 - 150, Screen.height / 2, 150, 150), "RETRY"))
         {
             state = State.Prepare;
@@ -134,8 +109,7 @@ public class SGGooseController : MonoBehaviour {
     }
     void Prepare()
     {
-        _body.transform.position = new Vector2(-4, 0);
-        _body.isKinematic = true;
+		GUI.Label (new Rect (Screen.width / 2, Screen.height / 2, 100, 20), "PREPARE TO DIE");
       
     }
 }
